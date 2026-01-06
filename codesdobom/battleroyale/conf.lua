ee871f1048aa3bc84bf898197bb7998ac3a989bd
Sam <0000114513595xsp@al.educacao.sp.gov.br>
2026-01-05 21:25:48 -0300

    Add files via upload

---
+++ codesdobom/battleroyale/conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+    t.title = "Mini Battle Royale"
+    t.version = "11.5"
+    t.window.width = 1280
+    t.window.height = 720
+    t.window.resizable = true
+    t.window.vsync = 1
+    t.window.icon = nil  
+end
