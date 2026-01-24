70b9e460e715a3da16f33842e3a370d40514be25
Sam <0000114513595xsp@al.educacao.sp.gov.br>
2026-01-24 10:57:39 -0300

    Delete battleroyale directory

--- battleroyale/conf.lua
+++
@@ -1,9 +0,0 @@
-function love.conf(t)
-    t.title = "Mini Battle Royale"
-    t.version = "11.5"
-    t.window.width = 1280
-    t.window.height = 720
-    t.window.resizable = true
-    t.window.vsync = 1
-    t.window.icon = nil  
-end
